0d56c21a9ba5d352d81a79f9c8c6b33bdbccace4
Mohamed Amine Bellil <mbellil262@gmail.com>
2025-01-19 17:55:00 +0100

    try fix issue

--- izr_server/settings.py
+++ izr_server/settings.py
@@ -15,11 +15,12 @@ from pathlib import Path
 from corsheaders.defaults import default_headers
 import environ
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 env = environ.Env()
-environ.Env.read_env()
+env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
